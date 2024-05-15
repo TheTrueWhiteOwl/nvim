dded6276eea9739c46a1c65b84d17f2b163a9fec
TheTrueWhiteOwl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2024-05-15 02:49:46 +0200

    feat: <esc> in n turns off hl after searching

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,37 +3,45 @@ vim.opt.timeoutlen = 1500
 
 vim.opt.ttimeout = false
 
+
+local cmd = vim.cmd
+
+-- Search highlights
+vim.keymap.set('n', '<Esc>', cmd.nohlsearch)
+
 -- Fugitive
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", cmd.Git)
 
 -- Telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files)
 
+local diagnostic = vim.diagnostic
 -- Diagnostics
-vim.keymap.set("n", "gl", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "gl", diagnostic.open_float)
+vim.keymap.set("n", "[d", diagnostic.goto_prev)
+vim.keymap.set("n", "]d", diagnostic.goto_next)
 
 -- LSPs
 vim.api.nvim_create_autocmd("LspAttach", {
    desc = "LSP actions",
    callback = function(event)
+      local buf = vim.lsp.buf
       local opts = {buffer = event.buf}
 
       -- these will be buffer-local keybindings
       -- because they only work if you have an active language server
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-      vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-      vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-      vim.keymap.set({"n", "x"}, "<F3>", function() vim.lsp.buf.format({async = true}) end, opts)
-      vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "K", buf.hover, opts)
+      vim.keymap.set("n", "gd", buf.definition, opts)
+      vim.keymap.set("n", "gD", buf.declaration, opts)
+      vim.keymap.set("n", "gi", buf.implementation, opts)
+      vim.keymap.set("n", "go", buf.type_definition, opts)
+      vim.keymap.set("n", "gr", buf.references, opts)
+      vim.keymap.set("n", "gs", buf.signature_help, opts)
+      vim.keymap.set("n", "<F2>", buf.rename, opts)
+      vim.keymap.set({"n", "x"}, "<F3>", function() buf.format({async = true}) end, opts)
+      vim.keymap.set("n", "<F4>", buf.code_action, opts)
    end
 })
 
@@ -48,4 +56,4 @@ cmp.mapping.preset.insert({
 })
 
 -- UndoTree
-vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>ut", cmd.UndotreeToggle)
