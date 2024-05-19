b140bcfe387475a5bad7db68a4d67a95472003e0
TheTrueWhiteOwl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2024-05-19 19:52:25 +0200

    feat: added json support to my config

---
+++ after/ftplugin/json.lua
@@ -0,0 +1,6 @@
+-- indentation
+vim.opt_local.shiftwidth = 2;    -- width used for auto indentation. When 0: shiftwidth = tabstop
+vim.opt_local.softtabstop = 2;   -- tabs of given width will be emulated using spaces and tabs
+
+vim.opt_local.expandtab = true;  -- use spaces for indentation
+
